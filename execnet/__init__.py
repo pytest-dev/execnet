80ed270db5ca373c31fa5c63a1f75c6092c4a8c7
holger krekel <holger@merlinux.eu>
2009-11-19 19:17:32 +0100

    bumping version to the envisioned 1.0.0b4, using setuptools if available

--- execnet/__init__.py
+++ execnet/__init__.py
@@ -4,7 +4,7 @@ package for connecting to local and remote Python Interpreters.
 
 (c) 2009, Holger Krekel and others
 """
-__version__ = "1.0.0b3"
+__version__ = "1.0.0b4"
 
 import execnet.apipkg
 
