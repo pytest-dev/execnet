9635abb3230f870cb83df0eb3908140eaac5c7b7
holger krekel <holger@merlinux.eu>
2009-11-23 17:27:46 +0100

    preparing final execnet-1.0.0

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def main():
         author='holger krekel and others',
         author_email='holger at merlinux.eu',
         classifiers=[
-            'Development Status :: 4 - Beta',
+            'Development Status :: 5 - Production/Stable',
             'Intended Audience :: Developers',
             'License :: OSI Approved :: GNU General Public License (GPL)',
             'Operating System :: POSIX',
