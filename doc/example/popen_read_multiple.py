c1a1f8f48269d5a9874df89ae074e025d6da6bf5
holger krekel <holger@merlinux.eu>
2009-12-06 18:00:30 +0100

    avoid deprecated PopenGateway, new Group.defaultspec example, some doc tweaks

--- doc/example/popen_read_multiple.py
+++ doc/example/popen_read_multiple.py
@@ -9,7 +9,7 @@ NUM_PROCESSES = 5
 
 channels = []
 for i in range(NUM_PROCESSES):
-    gw = execnet.PopenGateway() # or use SSH or socket gateways 
+    gw = execnet.makegateway() # or use SSH or socket gateways 
     channel = gw.remote_exec("""
         import time
         secs = channel.receive()
