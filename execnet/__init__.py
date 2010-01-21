749407085ba03e059eb1fd7179026b107e3f2a30
holger krekel <holger@merlinux.eu>
2010-01-21 20:19:29 +0100

    just silence shutdown related exceptions at receiver-thread end (sigh)

--- execnet/__init__.py
+++ execnet/__init__.py
@@ -3,7 +3,7 @@ execnet: pure python lib for connecting to local and remote Python Interpreters.
 
 (c) 2010, Holger Krekel and others
 """
-__version__ = "1.0.4"
+__version__ = "1.0.4post1"
 
 import execnet.apipkg
 
