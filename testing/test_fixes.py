35aaa07009730aa84fe6451228ef074a16a4367c
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2012-09-04 09:32:09 +0200

    fix issue 8 - don't kill remote pids on local jython

---
+++ testing/test_fixes.py
@@ -0,0 +1,27 @@
+from execnet import Group
+from execnet.gateway_bootstrap import fix_pid_for_jython_popen
+
+def test_jython_bootstrap_fix():
+    group = Group()
+    gw = group.makegateway('popen')
+    popen = gw._io.popen
+    real_pid = popen.pid
+    try:
+        # nothing happens when calling it on a normal seyup
+        fix_pid_for_jython_popen(gw)
+        assert popen.pid == real_pid
+
+        # if there is no pid for a popen gw, restore
+        popen.pid = None
+        fix_pid_for_jython_popen(gw)
+        assert popen.pid == real_pid
+
+        # if there is no pid for other gw, ignore - they are remote
+        gw.spec.popen = False
+        popen.pid = None
+        fix_pid_for_jython_popen(gw)
+        assert popen.pid is None
+
+    finally:
+        popen.pid = real_pid
+        group.terminate(timeout=1)
