7bed3ddeb705eed1e05dfcd74ac88d8aa8a21769
Christian Theune <ct@flyingcircus.io>
2015-03-08 17:08:32 +0100

    Implement a separate vagrant connection type to channel SSH through the vagrant mechanism to deal with managed key pairs and transaparent port forwarding.
    Can be used via makegateway("vagrant=<machine>") and accepts all additional
    SSH options as SSH does.
    
    The test is a bit annoying as it currently demonstrates this working by
    actually firing up vagrant. This means it a) needs vagrant and virtualbox and
    b) it downloads a relatively large image when running the tests the first time
    and c) it takes quite a few seconds running the test at all times.

---
+++ doc/example/conftest.py
@@ -0,0 +1,13 @@
+
+import py, sys
+
+# make execnet and example code importable
+cand = py.path.local(__file__).dirpath().dirpath().dirpath()
+if cand.join("execnet", "__init__.py").check():
+    if str(cand) not in sys.path:
+        sys.path.insert(0, str(cand))
+cand = py.path.local(__file__).dirpath()
+if str(cand) not in sys.path:
+    sys.path.insert(0, str(cand))
+
+pytest_plugins = ['doctest']
