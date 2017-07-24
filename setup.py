a88b89a574d22e28ab1052eb1a45479d1aebdde9
Ronny Pfannschmidt <ronny.pfannschmidt@redhat.com>
2017-07-24 21:52:01 +0200

    expand readme to someting more comprehensible

--- setup.py
+++ setup.py
@@ -1,46 +1,11 @@
-"""
-execnet: distributed Python deployment and communication
-========================================================
-
-.. _execnet: http://codespeak.net/execnet
-
-execnet_ provides carefully tested means to ad-hoc interact with Python
-interpreters across version, platform and network barriers.  It provides
-a minimal and fast API targetting the following uses:
-
-* distribute tasks to local or remote processes
-* write and deploy hybrid multi-process applications
-* write scripts to administer multiple hosts
-
-Features
-------------------
-
-* zero-install bootstrapping: no remote installation required!
-
-* flexible communication: send/receive as well as
-  callback/queue mechanisms supported
-
-* simple serialization of python builtin types (no pickling)
-
-* grouped creation and robust termination of processes
-
-* well tested between CPython 2.6-3.X, Jython 2.5.1 and PyPy 2.2
-  interpreters.
-
-* interoperable between Windows and Unix-ish systems.
-
-* integrates with different threading models, including standard
-  os threads, eventlet and gevent based systems.
-
-"""
-
-
 def main():
     from setuptools import setup
+    with open("README.txt") as fp:
+        readme = fp.read()
     setup(
         name='execnet',
         description='execnet: rapid multi-Python deployment',
-        long_description=__doc__,
+        long_description=readme,
         use_scm_version={'write_to': 'execnet/_version.py'},
         url='http://codespeak.net/execnet',
         license='MIT',
@@ -63,6 +28,7 @@ def main():
         install_requires=['apipkg>=1.4'],
         setup_requires=['setuptools_scm'],
     )
+
 
 if __name__ == '__main__':
     main()
